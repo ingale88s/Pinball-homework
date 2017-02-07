a901722af75f5a3f9c3c90193788c750c687cc7c
Sandeep Ingale <sandeepingale.18@gmail.com>
2017-02-06 19:55:12 -0600

    second commit
    This commit has most of the changes. now Pinball game is complete

---
+++ Assets/paddleLeft.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddleLeft : MonoBehaviour
+{
+
+    public static HingeJoint2D leftPaddle;
+    public static JointMotor2D motor;
+
+    // Use this for initialization
+    void Start()
+    {
+        leftPaddle = gameObject.GetComponent<HingeJoint2D>();
+        motor = leftPaddle.motor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            motor.motorSpeed = -5000;
+            leftPaddle.useMotor = true;
+            leftPaddle.motor = motor;
+        }
+        else
+        {
+            motor.motorSpeed = 0;
+            leftPaddle.useMotor = false;
+        }
+    }
+}
---
+++ Assets/paddleRight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddleRight : MonoBehaviour {
+
+    public static HingeJoint2D rightPaddle;
+    public static JointMotor2D motor;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rightPaddle = gameObject.GetComponent<HingeJoint2D>();
+        motor = rightPaddle.motor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            motor.motorSpeed = 5000;
+            rightPaddle.useMotor = true;
+            rightPaddle.motor = motor;
+        }
+        else
+        {
+            motor.motorSpeed = 0;
+            rightPaddle.useMotor = false;
+        }
+    }
+}
